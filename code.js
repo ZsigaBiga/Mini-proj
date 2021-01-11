0affb4216c9fd5994b344ba5fdd582ecf0e5bf8a
Szász Áron <szaszjozsefg9@gmail.com>
2021-01-11 10:32:50 +0100

    Base-line

--- code.js
+++ code.js
@@ -1,15 +1,20 @@
 'use strict'
 
-function perc2color(perc) {
+function atmenet() {
+
+	let szam = document.getElementById('beszam').value;
+
 	var r, g, b = 0;
-	if(perc < 50) {
+	if(szam < 50) {
 		r = 255;
-		g = Math.round(5.1 * perc);
+		g = Math.round(5.1 * szam);
 	}
 	else {
 		g = 255;
-		r = Math.round(510 - 5.10 * perc);
+		r = Math.round(510 - 5.10 * szam);
 	}
 	var h = r * 0x10000 + g * 0x100 + b * 0x1;
-	return '#' + ('000000' + h.toString(16)).slice(-6);
+
+	console.log(beszam);
+	return document.getElementById('szin').innerHTML += '#' + ('000000' + h.toString(16)).slice(-6);
 }
